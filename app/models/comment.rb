3c82c9a8f367d0a2d37de38b32c00f4c6cabbe27
Paul Han <paulhan221@gmail.com>
2015-04-19 15:50:41 -0400

    added icons to profiles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
 
+  def commenter
+    self.commentable.itinerary.user
+  end
+
   def commenter_name
     self.commentable.itinerary.user.name
   end
