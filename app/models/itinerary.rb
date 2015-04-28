b776d63642f5c94f7a7b24ef6eaf09e8f59f7478
github username <github email address>
2015-04-28 11:05:24 -0400

    on mouseover, display name and address of itinerary item

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -9,23 +9,22 @@ class Itinerary < ActiveRecord::Base
   has_many :itinerary_items
   validates_presence_of :title, :user_id
 
-
   def res_pins
     reservations = self.reservations.collect do |reservation, json|
-      [reservation.id, reservation.hotel.latitude, reservation.hotel.longitude]
-    end.to_json
+      [reservation.id, reservation.hotel.name, reservation.hotel.address, reservation.hotel.latitude, reservation.hotel.longitude]
+    end.to_json.html_safe
   end
 
   def meal_pins
     meal = self.meals.collect do |meal, json|
-      [meal.id, meal.restaurant.latitude, meal.restaurant.longitude]
-    end.to_json
+      [meal.id, meal.restaurant.name, meal.restaurant.address, meal.restaurant.latitude, meal.restaurant.longitude]
+    end.to_json.html_safe
   end
 
   def activity_pins
     activities = self.activities.collect do |activity, json|
-      [activity.id, activity.attraction.latitude, activity.attraction.longitude]
-    end.to_json
+      [activity.id, activity.attraction.name, activity.attraction.address, activity.attraction.latitude, activity.attraction.longitude]
+    end.to_json.html_safe
   end
 
   def itinerary_display_pic?
