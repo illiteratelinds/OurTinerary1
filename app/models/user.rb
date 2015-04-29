d322912559100cdbdcd13fb2f4f4855a60af26e0
github username <github email address>
2015-04-29 16:44:14 -0400

    fixed some stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ActiveRecord::Base
   has_many :restaurants, through: :itineraries
   has_many :friendships
   has_many :comments
+
   has_many :passive_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :active_friends, -> { where(friendships: { status: true}) }, :through => :friendships, :source => :friend
   has_many :passive_friends, -> { where(friendships: { status: true}) }, :through => :passive_friendships, :source => :user
   has_many :pending_friends, -> { where(friendships: { status: false}) }, :through => :friendships, :source => :friend
   has_many :requested_friendships, -> { where(friendships: { status: false}) }, :through => :passive_friendships, :source => :user
+  
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :reviews, foreign_key: :creator_id
