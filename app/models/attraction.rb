8e9b2b32cfc4faad403f059f79b884b77f166164
github username <github email address>
2015-04-30 17:53:47 -0400

    added timelines to restaurant & attractions

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,6 +3,7 @@ class Attraction < ActiveRecord::Base
   has_many :itineraries, through: :attractions
   has_many :users
   has_many :wishlists, as: :wishlistable
+  has_many :reviews, through: :activities
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ActiveRecord::Base
   has_many :itineraries, through: :meals
   has_many :users
   has_many :wishlists, as: :wishlistable
+  has_many :reviews, through: :meals
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 end
